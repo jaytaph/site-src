28686652ac7d5dbc5b7d2a19c9024b5ab16d0521
Jibé Barth <barth.jib@gmail.com>
2020-04-05 17:35:42 +0200

    Improve IndexController

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -1,17 +1,30 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
-class IndexController extends AbstractController
+final class IndexController
 {
+    /**
+     * @var \Twig\Environment
+     */
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(): Response
     {
-        return $this->render('index.html.twig');
+        return new Response($this->twig->render('index.html.twig'));
     }
 }
