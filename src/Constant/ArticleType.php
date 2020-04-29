26bd579350c27cf9a3a7619b2888178dc61ab005
Jibé Barth <barth.jib@gmail.com>
2020-04-29 14:59:51 +0200

    Create article model

---
+++ src/Constant/ArticleType.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Constant;
+
+final class ArticleType
+{
+    public const EXTERNAL = 'external';
+    public const INTERNAL = 'internal';
+    public const GIST = 'gist';
+}
