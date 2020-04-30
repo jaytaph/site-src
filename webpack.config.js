88bc129e2c438492cba3d2ad80dbcdc54c37dfef
Jibé Barth <barth.jib@gmail.com>
2020-04-30 18:14:07 +0200

    Set NOD_ENV to production when Encore is production

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,10 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
+if (Encore.isProduction()) {
+    process.env.NODE_ENV = 'production';
+}
+
 Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/dist/')
