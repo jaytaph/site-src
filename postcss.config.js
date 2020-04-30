e6849d4a3cfc6035d05cde73eb693457570035c0
Jibé Barth <barth.jib@gmail.com>
2020-04-30 18:27:37 +0200

    Add purgecss on other files (ie FontAwesome)

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,13 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: ['./templates/*.html.twig'],
+    whitelist: ['emoji']
+})
+
 module.exports = {
     plugins: [
+        ...process.env.NODE_ENV === 'production'
+            ? [purgecss]
+            : [],
         require('tailwindcss'),
     ]
 }
