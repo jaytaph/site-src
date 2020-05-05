64226ca8e4175f413d2139c227da9cf2730c9029
Jibé Barth <barth.jib@gmail.com>
2020-05-05 15:12:50 +0200

    Add share function

--- assets/js/gist.js
+++ assets/js/gist.js
@@ -5,3 +5,26 @@ import Prism from 'prismjs'
 import 'prismjs/themes/prism-tomorrow.css'
 
 Prism.highlightAll();
+
+document.addEventListener('DOMContentLoaded', function() {
+// Share button
+if (navigator.share) {
+    const shareBtn = document.getElementsByClassName('share');
+    shareBtn.forEach(function (btn) {
+        btn.classList.remove('hidden');
+        const dataToShare = { title: btn.getAttribute('data-title')};
+        let url = document.location.href;
+        const canonicalElement = document.querySelector('link[rel=canonical]');
+        if (canonicalElement !== null) {
+            url = canonicalElement.href;
+        }
+        dataToShare.url = url;
+        btn.onclick = function(e) {
+            e.preventDefault();
+            navigator.share(dataToShare)
+                .then(() => console.log('Successful share'))
+                .catch((error) => console.log('Error sharing', error));
+        };
+    })
+
+}}, false);
