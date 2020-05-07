2d489243fac77229d59caa7594089833759e29c8
Jibé Barth <barth.jib@gmail.com>
2020-05-07 10:08:48 +0200

    Assert url host is in punycode for meta

---
+++ src/Twig/UrlSanitizerExtension.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use function Symfony\Component\String\u;
+
+final class UrlSanitizerExtension extends AbstractExtension
+{
+    /**
+     * @return array<\Twig\TwigFilter>
+     */
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('sanitize_url', [$this, 'sanitizeUrl']),
+        ];
+    }
+
+    public function sanitizeUrl(string $url): string
+    {
+        $host = \Safe\parse_url($url, PHP_URL_HOST);
+        if (null === $host) {
+            throw new \InvalidArgumentException(\sprintf('"%s" is not an absolute url', $url));
+        }
+
+        $sanitizedHost = \idn_to_ascii($host, IDNA_NONTRANSITIONAL_TO_ASCII, INTL_IDNA_VARIANT_UTS46);
+        if (false === $sanitizedHost) {
+            $sanitizedHost = \mb_strtolower($host);
+        }
+
+        return u($url)->replace($host, $sanitizedHost)->toString();
+    }
+}
