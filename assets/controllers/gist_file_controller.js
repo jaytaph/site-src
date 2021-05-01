215ce1dc200b83ffa02785e2acbc80c77d7d0f8a
Jibé Barth <barth.jib@gmail.com>
2021-05-01 14:06:06 +0200

    [ux] add stimulus controller to render gist files (#33)

---
+++ assets/controllers/gist_file_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from 'stimulus';
+
+import jsonp from "jsonp";
+
+const gist_css = 'https://github.githubassets.com/assets/gist-embed-b4e1b64ab37d0cf7698bb0c5008263fe.css';
+
+/* stimulusFetch: 'lazy' */
+export default class extends Controller {
+    connect() {
+        console.log(this.articleValue, this.fileValue, this.githubUserValue)
+        const gistLink = `https://gist.github.com/${this.githubUserValue}/${this.articleValue}.json?file=${this.fileValue}`;
+
+        jsonp(gistLink, { timeout: 20000 }, (err, response) => {
+            if (err) {
+                console.warn('unable to retrieve gist content');
+                return;
+            }
+            this.element.innerHTML = `
+                <link rel="stylesheet" href="${gist_css}">
+                ${response.div}
+            `;
+        });
+    }
+
+    static values = { article: String, file: String, githubUser: String }
+}
