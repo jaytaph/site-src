daf147861aa512ee46f67ab722509a011d8bfba1
Jibé Barth <barth.jib@gmail.com>
2023-05-07 17:41:30 +0200

    AsCommand in GenerateReadmeCommand

--- src/Command/GenerateReadmeCommand.php
+++ src/Command/GenerateReadmeCommand.php
@@ -5,28 +5,25 @@ declare(strict_types=1);
 namespace App\Command;
 
 use App\Generator\ReadmeGenerator;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
+#[AsCommand(name: 'generate-readme')]
 final class GenerateReadmeCommand extends Command
 {
-    private const NAME = 'generate-readme';
-
-    private ReadmeGenerator $readmeGenerator;
-
-    public function __construct(ReadmeGenerator $readmeGenerator)
+    public function __construct(private ReadmeGenerator $readmeGenerator)
     {
-        parent::__construct(self::NAME);
-        $this->readmeGenerator = $readmeGenerator;
+        parent::__construct();
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $style = new SymfonyStyle($input, $output);
         $this->readmeGenerator->generate();
-        $style->success('Readme successfuly generated');
+        $style->success('Readme successfully generated');
 
         return self::SUCCESS;
     }
